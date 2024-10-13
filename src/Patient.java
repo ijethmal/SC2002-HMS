742d97265eeb13bd11a3e61da09b6e017309abc2
Isha <s10205038@connect.np.edu.sg>
2024-10-13 18:18:55 +0800

    added patient class

---
+++ src/Patient.java
@@ -0,0 +1,15 @@
+public class Patient {
+
+    String name;
+    int age;
+
+    public Patient(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
