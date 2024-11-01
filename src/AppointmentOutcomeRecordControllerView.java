d18ee6eb17b556ffc08b60078042ce3eda21fb2f
Roopashanthoshini <166206827+Roopashanthoshini@users.noreply.github.com>
2024-11-02 01:10:03 +0800

    Create AppointmentOutcomeRecordControllerView.java

---
+++ src/AppointmentOutcomeRecordControllerView.java
@@ -0,0 +1,40 @@
+public class AppointmentOutcomeRecordControllerView {
+    private AppointmentOutcomeRecordModel model;
+
+    public AppointmentOutcomeRecordControllerView(AppointmentOutcomeRecordModel model) {
+        this.model = model;
+    }
+
+    public void viewRecord() {
+        System.out.println("---- Appointment Outcome Record ----");
+        System.out.println("Record ID: " + model.getRecordID());
+        System.out.println("Patient ID: " + model.getPatientId());
+        System.out.println("Date and Time: " + model.getDateTime());
+        System.out.println("Type of Service: " + model.getTypeOfService());
+        System.out.println("-------------------------------------");
+    }
+
+    public void viewDiagnoses() {
+        System.out.println("---- Diagnoses ----");
+        if (model.getDiagnoses().isEmpty()) {
+            System.out.println("No diagnoses available.");
+        } else {
+            for (Diagnosis diagnosis : model.getDiagnoses()) {
+                System.out.println(diagnosis);
+            }
+        }
+        System.out.println("--------------------");
+    }
+
+    public void viewPrescriptions() {
+        System.out.println("---- Prescriptions ----");
+        if (model.getPrescriptions().isEmpty()) {
+            System.out.println("No prescriptions available.");
+        } else {
+            for (Prescription prescription : model.getPrescriptions()) {
+                System.out.println(prescription);
+            }
+        }
+        System.out.println("-----------------------");
+    }
+}
