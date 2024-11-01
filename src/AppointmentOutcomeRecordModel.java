d58202dc85493449162136258e25ccad237fd9ed
Roopashanthoshini <166206827+Roopashanthoshini@users.noreply.github.com>
2024-11-02 01:11:41 +0800

    Update AppointmentOutcomeRecordModel.java

--- src/AppointmentOutcomeRecordModel.java
+++ src/AppointmentOutcomeRecordModel.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
-
+ 
 public class AppointmentOutcomeRecordModel {
     private String recordID;
     private String patientId;
