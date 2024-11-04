9aa5e39ec0e727c59d3c00a793e7beaef2874ab3
kavya-110 <kavyasrivastava1101@gmail.com>
2024-11-04 16:45:27 +0800

    Update Patient.java

--- src/Patient.java
+++ src/Patient.java
@@ -1,14 +1,1 @@
-public class Patient {
-    private String id;
-    private String password;
 
-    public Patient(String id, String password) {
-        this.id = id;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Patient ID: " + id + ", Password: " + password;
-    }
-}
